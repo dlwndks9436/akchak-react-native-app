0ce9061f8c948c605f0d9b935fef1077ecd5cb21
dlwndks9436 <dlwndks9436@naver.com>
2022-01-14 14:08:24 +0900

    upgrade sdk version 31

--- App.tsx
+++ App.tsx
@@ -2,11 +2,7 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import Home from './Home';
-import {
-  StatusBar,
-  // StyleSheet,
-  useColorScheme,
-} from 'react-native';
+
 import CameraView from './CameraView';
 
 export type RootStackParamList = {
@@ -17,19 +13,14 @@ export type RootStackParamList = {
 const RootStack = createStackNavigator<RootStackParamList>();
 
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
   return (
     <NavigationContainer>
       <RootStack.Navigator initialRouteName="Home">
-        <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
         <RootStack.Screen name="Home" component={Home} />
         <RootStack.Screen name="Camera" component={CameraView} />
       </RootStack.Navigator>
     </NavigationContainer>
   );
 };
-
-// const styles = StyleSheet.create({});
 
 export default App;
--- Home.tsx
+++ Home.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import {View, Text, Button} from 'react-native';
+import {View, Text, Button, StatusBar, useColorScheme} from 'react-native';
 import {RootStackParamList} from './App';
 import {StackScreenProps} from '@react-navigation/stack';
 
 type Props = StackScreenProps<RootStackParamList, 'Home'>;
 
 export default function Home({navigation}: Props) {
+  const isDarkMode = useColorScheme() === 'dark';
   return (
     <View>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
       <Text>This is home.</Text>
       <Button
         title="Camera"
