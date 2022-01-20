21c6cf047f2b9bcad0daa197884da6e9ea31d180
dlwndks9436 <dlwndks9436@naver.com>
2022-01-20 15:57:57 +0900

    Implement video control

--- src/screens/VideoTrimScreen.tsx
+++ src/screens/VideoTrimScreen.tsx
@@ -1,52 +1,62 @@
-import React, {useRef} from 'react';
+import React, {Component} from 'react';
 import {View, StyleSheet} from 'react-native';
-import Video from 'react-native-video';
-import {useAndroidBackHandler} from 'react-navigation-backhandler';
+// import Video from 'react-native-video';
+import {AndroidBackHandler} from 'react-navigation-backhandler';
 import {Alert} from 'react-native';
-import {RootStackTrimScreenProps} from '../types/type';
+import {RootStackTrimScreenProps, RootStackParamList} from '../types/type';
+import VideoPlayer from 'react-native-video-controls';
+import {StackNavigationProp} from '@react-navigation/stack';
 
 //handle back button to home!
 
-export default function VideoTrimScreen({
-  navigation,
-  route,
-}: RootStackTrimScreenProps) {
-  const player = useRef<Video>(null);
+export default class VideoTrimScreen extends Component {
+  // const player = useRef<Video>(null);
+  videoUri: string;
+  navigation: StackNavigationProp<RootStackParamList, 'VideoTrim'>;
 
-  const {videoUri} = route.params;
+  constructor(props: RootStackTrimScreenProps) {
+    super(props);
+    console.log(props.route);
+    this.videoUri = props.route.params.videoUri;
 
-  const checkGoBack = () => {
+    this.navigation = props.navigation;
+    this.checkGoBack = this.checkGoBack.bind(this);
+    this.onBackButtonPressAndroid = this.onBackButtonPressAndroid.bind(this);
+  }
+
+  checkGoBack = () => {
     Alert.alert('Would you discard this practice?', undefined, [
       {text: 'Cancel', style: 'cancel'},
       {
         text: 'OK',
         onPress: () => {
-          navigation.reset({index: 0, routes: [{name: 'Tab'}]});
+          this.navigation.reset({index: 0, routes: [{name: 'Tab'}]});
         },
       },
     ]);
   };
 
-  useAndroidBackHandler(() => {
-    checkGoBack();
-    return true;
-  });
+  onBackButtonPressAndroid = () => {
+    /*
+     *   Returning `true` from `onBackButtonPressAndroid` denotes that we have handled the event,
+     *   and react-navigation's lister will not get called, thus not popping the screen.
+     *
+     *   Returning `false` will cause the event to bubble up and react-navigation's listener will pop the screen.
+     * */
 
-  return (
-    <View style={styles.body}>
-      <Video
-        source={{uri: videoUri}} // Can be a URL or a local file.
-        ref={player} // Store reference
-        onBuffer={buff => {
-          console.log(buff);
-        }} // Callback when remote video is buffering
-        onError={err => {
-          console.log(err);
-        }} // Callback when video cannot be loaded
-        style={styles.backgroundVideo}
-      />
-    </View>
-  );
+    this.checkGoBack();
+    return true;
+  };
+
+  render(): React.ReactNode {
+    return (
+      <AndroidBackHandler onBackPress={this.onBackButtonPressAndroid}>
+        <View style={styles.body}>
+          <VideoPlayer source={{uri: this.videoUri}} />
+        </View>
+      </AndroidBackHandler>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
